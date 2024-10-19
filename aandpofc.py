00de0d14382f60743bbefb72d62e691454837df5
aswin <aswin4147@gmail.com>
2024-10-19 23:02:31 +0530

    Did programs 1, 2, 3, 4

---
+++ aandpofc.py
@@ -0,0 +1,6 @@
+# program to find the area and perimeter of a circle
+
+radius = int(input("Enter the radius: "))
+area = 3.14 * (radius ** 2)
+perimeter = 2 * 3.14 * radius
+print(f"The area is {area} and the perimeter is {perimeter}")
