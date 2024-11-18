86b08de6e2c187f2fade4ba8d42ff9adcfb68082
notmaal <aswin4147@gmail.com>
2024-11-18 15:46:21 +0530

    added dictionary programs

--- LabCycle1/14.py
+++ LabCycle1/14.py
@@ -1,1 +1,4 @@
 # sort dictionary
+dictionary = {"pineapple" : 23, "apple" : 2, "banana" : 32}
+print(f"The dictionary in sorted order is: {dict(sorted(dictionary.items()))}")
+print(f"The dictionary in sorted descending order is: {dict(sorted(dictionary.items(), reverse=True))}")
--- LabCycle1/15.py
+++ LabCycle1/15.py
@@ -1,1 +1,5 @@
 # merge two dictionary
+dictionary1 = {"pineapple" : 23, "apple" : 2, "banana" : 32}
+dictionary2 = {"orange" : 65, "grapes" : 4, "watermelon" : 78}
+dictionary1.update(dictionary2)
+print(dictionary1)
